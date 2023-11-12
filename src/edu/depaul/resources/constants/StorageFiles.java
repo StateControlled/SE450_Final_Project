66c5fb45c01ce39d2d4bdc9ffe09d8e69c4d17e6
William Berthouex <william.berthouex@gmail.com>
2023-11-11 18:49:54 -0600

    Files
    Added a Class for some constant filenames.

---
+++ src/edu/depaul/resources/constants/StorageFiles.java
@@ -0,0 +1,9 @@
+package edu.depaul.resources.constants;
+
+import java.io.File;
+
+public class StorageFiles {
+        public static final File CATALOGUE_SOURCE = new File("src\\edu\\depaul\\resources\\itemcatalogue.json");
+        public static final File TEST_CATALOGUE = new File("src\\edu\\depaul\\resources\\testcatalogue.json");
+        public static final File USER_DATABASE = new File("src\\edu\\depaul\\resources\\userdatabase.json");
+}
--- src/edu/depaul/test/CatalogueTest.java
+++ src/edu/depaul/test/CatalogueTest.java
@@ -2,15 +2,16 @@ package edu.depaul.test;
 import java.util.ArrayList;
 
 import edu.depaul.item.AbstractItem;
+import edu.depaul.resources.constants.StorageFiles;
 import edu.depaul.catalogue.Catalogue;
 import java.io.File;
 
 public class CatalogueTest {
 
     public static void main(String[] args) {
-        File catalogue = new File("src\\edu\\depaul\\resources\\itemcatalogue.json");
+        File catalogue = StorageFiles.CATALOGUE_SOURCE;
+        File testcat = StorageFiles.TEST_CATALOGUE;
 
-        File testcat = new File("src\\edu\\depaul\\resources\\testcatalogue.json");
         System.out.println("CATALOGUE : " + catalogue.toPath());
 
         Catalogue C = new Catalogue(catalogue);
